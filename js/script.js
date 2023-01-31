3ce69de7ab7849b7765f1a7c826fc4374c0a5cd6
YouKlike <w93816@gmail.com>
2023-01-31 23:57:58 +0800

    add navigation bar.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,10 @@
-const navbar = document.querySelector("#nav");
-const navBtn = document.querySelector("#nav-btn");
-const closeBtn = document.querySelector("#close-btn");
-const sidebar = document.querySelector("#sidebar");
-const date = document.querySelector("#date");
-// add fixed class to navbar
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 80) {
-    navbar.classList.add("navbar-fixed");
-  } else {
-    navbar.classList.remove("navbar-fixed");
-  }
-});
-// show sidebar
-navBtn.addEventListener("click", function () {
-  sidebar.classList.add("show-sidebar");
-});
-closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
-});
-// set year
-date.innerHTML = new Date().getFullYear();
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
